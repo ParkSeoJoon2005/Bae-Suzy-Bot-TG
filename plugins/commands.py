0aa14d89cdeb941efa361a61169d6b01ad12bab3
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2021-10-30 22:57:23 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -21,8 +21,8 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/EvaMariaUpdates')
+            InlineKeyboardButton('🔍 Search', url='http://t.me/spaciousuniversegroup1'),
+            InlineKeyboardButton('🎟 Updates', url='http://t.me/Series2day')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
@@ -64,8 +64,8 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/EvaMariaUpdates')
+            InlineKeyboardButton('🔍 Search', url='http://t.me/spaciousuniversegroup1'),
+            InlineKeyboardButton('🎟 Updates', url='http://t.me/Series2day')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')

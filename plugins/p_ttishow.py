a2186ee85a8ed0e3c5101d71f3bb39baafb27859
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2021-10-31 21:10:32 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -42,7 +42,7 @@ async def save_group(bot, message):
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
-            text=f"<b>Thankyou For Adding Me In {message.chat.title} ❣️\n\nIf you have any questions & doubts about using me contact support.</b>",
+            text=f"<b>Thank you For Adding Me In {message.chat.title} ❣️\n\nPlz promote me as Admin ,After that i can Reply requests.</b>",
             reply_markup=reply_markup)
     else:
         for u in message.new_chat_members:

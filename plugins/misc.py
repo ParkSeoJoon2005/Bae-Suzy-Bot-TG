8065db6caf090e383a7fc08e5c7e6c3cfee80aff
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2021-11-09 00:32:05 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -1,6 +1,7 @@
 import os
 from pyrogram import Client, filters
-from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
+from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
+from info import IMDB_TEMPLATE
 from utils import extract_user, get_file_id, get_poster, last_online
 import time
 from datetime import datetime
@@ -82,49 +83,49 @@ async def who_is(client, message):
     message_out_str += f"<b>➲User Name:</b> @{username}\n"
     message_out_str += f"<b>➲User 𝖫𝗂𝗇𝗄:</b> <a href='tg://user?id={from_user.id}'><b>Click Here</b></a>\n"
     if message.chat.type in (("supergroup", "channel")):
-            try:
-                chat_member_p = await message.chat.get_member(from_user.id)
-                joined_date = datetime.fromtimestamp(
-                    chat_member_p.joined_date or time.time()
-                ).strftime("%Y.%m.%d %H:%M:%S")
-                message_out_str += (
-                    "<b>➲Joined this Chat on:</b> <code>"
-                    f"{joined_date}"
-                    "</code>\n"
-                )
-            except UserNotParticipant:
-                pass
-        chat_photo = from_user.photo
-        if chat_photo:
-            local_user_photo = await client.download_media(
-                message=chat_photo.big_file_id
+        try:
+            chat_member_p = await message.chat.get_member(from_user.id)
+            joined_date = datetime.fromtimestamp(
+                chat_member_p.joined_date or time.time()
+            ).strftime("%Y.%m.%d %H:%M:%S")
+            message_out_str += (
+                "<b>➲Joined this Chat on:</b> <code>"
+                f"{joined_date}"
+                "</code>\n"
             )
-            buttons = [[
-                InlineKeyboardButton('🔐 Close', callback_data='close_data')
-            ]]
-            reply_markup = InlineKeyboardMarkup(buttons)
-            await message.reply_photo(
-                photo=local_user_photo,
-                quote=True,
-                reply_markup=reply_markup,
-                caption=message_out_str,
-                parse_mode="html",
-                disable_notification=True
-            )
-            os.remove(local_user_photo)
-        else:
-            buttons = [[
-                InlineKeyboardButton('🔐 Close', callback_data='close_data')
-            ]]
-            reply_markup = InlineKeyboardMarkup(buttons)
-            await message.reply_text(
-                text=message_out_str,
-                reply_markup=reply_markup,
-                quote=True,
-                parse_mode="html",
-                disable_notification=True
-            )
-        await status_message.delete()
+        except UserNotParticipant:
+            pass
+    chat_photo = from_user.photo
+    if chat_photo:
+        local_user_photo = await client.download_media(
+            message=chat_photo.big_file_id
+        )
+        buttons = [[
+            InlineKeyboardButton('🔐 Close', callback_data='close_data')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await message.reply_photo(
+            photo=local_user_photo,
+            quote=True,
+            reply_markup=reply_markup,
+            caption=message_out_str,
+            parse_mode="html",
+            disable_notification=True
+        )
+        os.remove(local_user_photo)
+    else:
+        buttons = [[
+            InlineKeyboardButton('🔐 Close', callback_data='close_data')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await message.reply_text(
+            text=message_out_str,
+            reply_markup=reply_markup,
+            quote=True,
+            parse_mode="html",
+            disable_notification=True
+        )
+    await status_message.delete()
 
 @Client.on_message(filters.command(["imdb", 'search']))
 async def imdb_search(client, message):
@@ -154,7 +155,7 @@ async def imdb_callback(bot: Client, query: CallbackQuery):
     btn = [
             [
                 InlineKeyboardButton(
-                    text=f"{imdb.get('title')} - {imdb.get('year')}",
+                    text=f"{imdb.get('title')}",
                     url=imdb['url'],
                 )
             ]

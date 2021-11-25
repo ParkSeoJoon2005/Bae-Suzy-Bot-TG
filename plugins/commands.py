a993cd783ad7b2aea114b978ac1d035314734e54
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2021-11-25 11:36:05 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,10 +1,4 @@
 import os
-import math
-import json
-import time
-import shutil
-import heroku3
-import requests
 
 import logging
 import random
@@ -15,59 +9,10 @@ from pyrogram.errors.exceptions.bad_request_400 import ChatAdminRequired
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from database.ia_filterdb import Media, get_file_details
 from database.users_chats_db import db
-from info import CHANNELS, ADMINS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION, LOG_CHANNEL, PICS, HEROKU_API_KEY
+from info import CHANNELS, ADMINS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION, LOG_CHANNEL, PICS
 from utils import get_size, is_subscribed, temp
 
 logger = logging.getLogger(__name__)
-
-@Client.on_message(filters.command("status"))
-async def bot_status(client,message):
-    if HEROKU_API_KEY:
-        try:
-            server = heroku3.from_key(HEROKU_API_KEY)
-
-            user_agent = (
-                'Mozilla/5.0 (Linux; Android 10; SM-G975F) '
-                'AppleWebKit/537.36 (KHTML, like Gecko) '
-                'Chrome/80.0.3987.149 Mobile Safari/537.36'
-            )
-            accountid = server.account().id
-            headers = {
-            'User-Agent': user_agent,
-            'Authorization': f'Bearer {Config.HEROKU_API_KEY}',
-            'Accept': 'application/vnd.heroku+json; version=3.account-quotas',
-            }
-
-            path = "/accounts/" + accountid + "/actions/get-quota"
-
-            request = requests.get("https://api.heroku.com" + path, headers=headers)
-
-            if request.status_code == 200:
-                result = request.json()
-
-                total_quota = result['account_quota']
-                quota_used = result['quota_used']
-
-                quota_left = total_quota - quota_used
-                
-                total = math.floor(total_quota/3600)
-                used = math.floor(quota_used/3600)
-                hours = math.floor(quota_left/3600)
-                minutes = math.floor(quota_left/60 % 60)
-                days = math.floor(hours/24)
-
-                usedperc = math.floor(quota_used / total_quota * 100)
-                leftperc = math.floor(quota_left / total_quota * 100)
-
-                quota_details = f"""
-**Heroku Account Status**
-> __You have **{total} hours** of free dyno quota available each month.__
-> __Dyno hours used this month__ ;
-        - **{used} hours**  ( {usedperc}% )
-> __Dyno hours remaining this month__ ;
-        - **{hours} hours**  ( {leftperc}% )
-        - **Approximately {days} days!**
-"""
 
                 
 @Client.on_message(filters.command("start"))
